b3c3ed4ee474c412f21331be52ceb759c24bb062
Arden <arden.haldorson@gmail.com>
2023-06-16 14:06:57 +0200

    added spotify list to the Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -20,6 +20,9 @@ const HomePage = () => {
 					<li><Link to={`/vehicles`}><span>Vehicles</span> <FontAwesomeIcon icon={faTruckPlane} /></Link></li>
 				</ul>
 			</div>
+			<div>
+				<iframe src="https://open.spotify.com/embed/playlist/04tSf4x1ntc0pYmC3mvDrf?utm_source=generator&theme=0" width="100%" height="152" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+			</div>
 		</>
 	)
 }
